cc83b1a4904a31c00abfaf61c42dea1e91ac7e3c
Thilina <thilina8madushanka@gmail.com>
2025-02-01 01:39:34 +0530

    user register complete and success

--- src/main/java/com/example/Instergram_clone_backend/controller/AuthController.java
+++ src/main/java/com/example/Instergram_clone_backend/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.example.Instergram_clone_backend.controller;
 import com.example.Instergram_clone_backend.exceptions.UserException;
 import com.example.Instergram_clone_backend.modal.User;
 import com.example.Instergram_clone_backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,10 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+    @Autowired
     private UserService userService;
 
-
-    @PostMapping("/signUp")
+    @PostMapping("/signup")
     public ResponseEntity<User> registerUserHandler(@RequestBody User user) throws UserException {
         User createdUser = userService.registerUser(user);
         return new ResponseEntity<User>(createdUser, HttpStatus.OK);
